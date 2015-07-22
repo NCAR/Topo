9bc14666b203a22eea4b7c9a5fb7b5bcb9a02432
PeterHjortLauritzen <pel@ucar.edu>
2015-07-22 15:12:11 -0600

    fix plotting script for lat lon grids

--- cube_to_target/ncl/plot-topo-vars.sh
+++ cube_to_target/ncl/plot-topo-vars.sh
@@ -1,1 +1,1 @@
-ncl  'area=-1' 'dataFile="../out.nc"' 'manual_levels=True' 'outputFile="topo-vars"' 'format="eps"' < plot-topo-vars.ncl
+ncl  'not_latlon=True' 'area=-1' 'dataFile="../out.nc"' 'manual_levels=True' 'outputFile="topo-vars"' 'format="eps"' < plot-topo-vars.ncl
