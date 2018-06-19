f9cb6c391f02507b0c2ae8990a557a780d30593f
Peter Lauritzen <pel@ucar.edu>
2018-06-19 09:10:22 -0600

    add script to copy setup and symbolic link large files

---
+++ copy_setup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+from_path="/project/amp/pel/TopoTrunk"
+to_path="/project/amp/pel/TopoCopyXXX"
+if [ -d "$to_path" ]; then
+    echo "$to_path already exists - ABORT"
+else
+echo "$to_path"
+mkdir $to_path
+cd $from_path
+tar zcvf $to_path/copy.tgz cube_to_target/*.F90 cube_to_target/Makefile *.make Makefile
+cd $to_path
+tar zxvf copy.tgz
+mkdir $to_path/cube_to_target/inputdata
+mkdir $to_path/cube_to_target/inputdata/grid-descriptor-file
+mkdir $to_path/cube_to_target/inputdata/namelist_defaults
+mkdir $to_path/cube_to_target/inputdata/smooth_topo_cube
+cd $to_path/cube_to_target/inputdata/grid-descriptor-file
+tmp=$from_path/cube_to_target/inputdata/grid-descriptor-file
+for i in $tmp/*.nc; do
+  echo "Symbolic link $i"
+  ln -s $i .
+done
+tmp=$from_path/cube_to_target/inputdata/smooth_topo_cube
+cd $to_path/cube_to_target/inputdata/smooth_topo_cube
+for i in $tmp/*.dat; do
+  echo "Symbolic link $i"
+  ln -s $i .
+done
+cd $to_path
+mkdir bin_to_cube
+cd bin_to_cube
+tmp=$from_path/bin_to_cube
+for i in $tmp/*.nc; do
+  echo "Symbolic link $i"
+  ln -s $i .
+done
+fi
+
