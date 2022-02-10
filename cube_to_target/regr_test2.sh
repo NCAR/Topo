d03713dda3156d0e0942ed51d5e303e4c52ef9ed
Peter Hjort Lauritzen <pel@ucar.edu>
2022-02-09 17:26:50 -0700

    low res var res test

--- cube_to_target/regr_test2.sh
+++ cube_to_target/regr_test2.sh
@@ -1,5 +1,8 @@
 #!/bin/tcsh
-./cube_to_target --grid_descriptor_file='/glade/p/cesmdata/inputdata/atm/cam/coords/ne0ARCTICne30x4_scrip_c191212.nc' --intermediate_cs_name='../regression-test-data/gmted2010_bedmachine-ncube0540.nc' --output_grid='ne-ARCTIC' --coarse_radius=012 --fine_radius=001 -p -r -y 4 -s
-set file=`ls -t1 output/*.nc |  head -n 1`
-echo $file
+./cube_to_target --grid_descriptor_file='../regression-test-data/ne0_15x2.nc' --intermediate_cs_name='../regression-test-data/gmted2010_bedmachine-ncube0540.nc' --output_grid='ne0_15x2_SA' --coarse_radius=012 --fine_radius=001 -p -r -y 2 -s
+
+
+#./cube_to_target --grid_descriptor_file='/glade/p/cesmdata/inputdata/atm/cam/coords/ne0ARCTICne30x4_scrip_c191212.nc' --intermediate_cs_name='../regression-test-data/gmted2010_bedmachine-ncube0540.nc' --output_grid='ne-ARCTIC' --coarse_radius=012 --fine_radius=001 -p -r -y 4 -s
+#set file=`ls -t1 output/*.nc |  head -n 1`
+#echo $file
 #/glade/p/cesm/cseg/tools/cprnc/cprnc -m $file ../regression-test-data/ne30pg3_nc0540_Nsw008_Nrs-001_Co012_Fi001_20220202.nc
