9ac4bd2080573d00649252cea7865eb1a6d89a60
Peter Hjort Lauritzen <pel@ucar.edu>
2022-02-18 17:14:09 -0700

    update baseline for ne30pg3 regression test

--- cube_to_target/regr_test1.sh
+++ cube_to_target/regr_test1.sh
@@ -2,4 +2,4 @@
 ./cube_to_target --grid_descriptor_file='../regression-test-data/ne30pg3.nc' --intermediate_cs_name='../regression-test-data/gmted2010_bedmachine-ncube0540.nc' --output_grid='ne30pg3' --coarse_radius=012 --fine_radius=001 -p -r -2 1
 set file=`ls -t1 output/*.nc |  head -n 1`
 echo $file
-/glade/p/cesm/cseg/tools/cprnc/cprnc -m $file ../regression-test-data/ne30pg3_nc0540_Nsw008_Nrs-001_Co012_Fi001_20220203.nc
+/glade/p/cesm/cseg/tools/cprnc/cprnc -m $file ../regression-test-data/ne30pg3_nc0540_Nsw008_Nrs000_Co012_Fi001_20220218.nc
