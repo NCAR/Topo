aeeb8ac36e788e01df9a8bfd59c785b7477e2636
Peter Lauritzen <pel@ucar.edu>
2022-05-09 11:55:44 -0600

    update regression tests

--- cube_to_target/regr_test3.sh
+++ cube_to_target/regr_test3.sh
@@ -4,4 +4,5 @@ make
 ./cube_to_target --grid_descriptor_file='../regression-test-data/fv0.9x1.25.nc'  --intermediate_cs_name='../regression-test-data/gmted2010_modis-ncube540.nc' --output_grid='fv0.9x1.25'  -u 'Peter Hjort Lauritzen, pel@ucar' -q 'output/' --smoothing_scale=100.0 -l 60 -r
 set file=`ls -t1 output/*.nc |  head -n 1`
 echo $file
-/glade/p/cesm/cseg/tools/cprnc/cprnc -m $file ../regression-test-data/fv0.9x1.25_gmted2010_bedmachine_nc0540_NoAniso_Laplace0060_20220504.nc
+#/glade/p/cesm/cseg/tools/cprnc/cprnc -m $file ../regression-test-data/fv0.9x1.25_gmted2010_bedmachine_nc0540_NoAniso_Laplace0060_20220504.nc
+/fs/cgd/csm/tools/cprnc/cprnc -m $file ../regression-test-data/fv0.9x1.25_gmted2010_bedmachine_nc0540_NoAniso_Laplace0060_20220504.nc
