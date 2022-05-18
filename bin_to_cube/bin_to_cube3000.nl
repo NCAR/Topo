ffbcb77351b9ae911001e0c29d3c47ffaf93486f
Peter Hjort Lauritzen <pel@ucar.edu>
2022-05-18 08:07:06 -0600

    minor mods

--- bin_to_cube/bin_to_cube3000.nl
+++ bin_to_cube/bin_to_cube3000.nl
@@ -1,5 +1,5 @@
 &binparams
-  raw_latlon_data_file = '/glade/p/cgd/amp/pel/topo/rawdata/gmted2010_modis-rawdata.nc'
-  output_file = 'gmted2010_modis-ncube3000.nc'
+  raw_latlon_data_file = '/glade/p/cgd/amp/pel/topo/rawdata/gmted2010_modis-rawdata-lonshift.nc'
+  output_file = 'gmted2010_modis_bedmahcine-ncube3000.nc'
   ncube=3000
 /
--- cube_to_target/regr_test1.sh
+++ cube_to_target/regr_test1.sh
@@ -3,6 +3,6 @@ make
 ./cube_to_target --grid_descriptor_file='../regression-test-data/ne30pg3.nc' --intermediate_cs_name='../regression-test-data/gmted2010_bedmachine-ncube0540.nc' --output_grid='ne30pg3' --smoothing_scale=100.0 -r -u 'Peter Hjort Lauritzen, pel@ucar.edu' -q 'output/' --grid_descriptor_file_gll='../regression-test-data/ne30np4.nc' -m -b
 set file=`ls -t1 output/*.nc |  head -n 1`
 echo $file
-#/glade/p/cesm/cseg/tools/cprnc/cprnc -m $file ../regression-test-data/ne30pg3_gmted2010_bedmachine_nc0540_Co012_20220505.nc
-/fs/cgd/csm/tools/cprnc/cprnc -m $file ../regression-test-data/ne30pg3_gmted2010_bedmachine_nc0540_Co011_20220511.nc
+/glade/p/cesm/cseg/tools/cprnc/cprnc -m $file ../regression-test-data/ne30pg3_gmted2010_bedmachine_nc0540_Co012_20220505.nc
+#/fs/cgd/csm/tools/cprnc/cprnc -m $file ../regression-test-data/ne30pg3_gmted2010_bedmachine_nc0540_Co011_20220511.nc
 #source plot.sh
