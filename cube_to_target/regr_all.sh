23ed70026cb0c9e38ba27deed5204f8f10bdc0fe
Peter Hjort Lauritzen <pel@ucar.edu>
2023-02-09 10:46:33 -0700

    update reference data for regression tests

--- cube_to_target/regr_all.sh
+++ cube_to_target/regr_all.sh
@@ -2,5 +2,5 @@
 make
 source regr_test1.sh
 source regr_test2.sh
-source regr_test2_lap.sh
+source regr_test2_old_smoother.sh
 source regr_test3.sh
--- cube_to_target/regr_test2_lap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/tcsh
-make
-./cube_to_target --grid_descriptor_file='../regression-test-data/ne0_15x2.nc' --intermediate_cs_name='../regression-test-data/gmted2010_bedmachine-ncube0540-220518.nc' --output_grid='ne0_15x2_SA' -y 2 -d -u "Peter Hjort Lauritzen, pel@ucar.edu" -m --smoothing_scale=200.0 -v
-set file=`ls -t1 output/*.nc |  head -n 1`
-echo $file
-/glade/p/cesm/cseg/tools/cprnc/cprnc -m $file ../regression-test-data/ne0_15x2_SA_gmted2010_bedmachine_nc0540_Laplace0200_20220610.nc
-#/fs/cgd/csm/tools/cprnc/cprnc  -m $file ../regression-test-data/ne0_15x2_SA_gmted2010_bedmachine_nc0540_Laplace0200_20220610.nc
-#source plot.sh
