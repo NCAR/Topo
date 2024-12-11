7ad8afb1f48d45d5e490362f96b0fcf4a15d6fa2
Peter Hjort Lauritzen <pel@ucar.edu>
2024-12-11 13:55:12 -0700

    update regressions tests to Casper

--- cube_to_target/regr_test2.sh
+++ cube_to_target/regr_test2.sh
@@ -3,6 +3,6 @@ make
 ./cube_to_target --grid_descriptor_file='../regression-test-data/ne0_15x2.nc' --intermediate_cs_name='../regression-test-data/gmted2010_bedmachine-ncube0540-220518.nc' --output_grid='ne0_15x2_SA' -y 2 -u "Peter Hjort Lauritzen, pel@ucar.edu" --smoothing_scale=200.0 -v
 set file=`ls -t1 output/*.nc |  head -n 1`
 echo $file
-/glade/p/cesm/cseg/tools/cprnc/cprnc -m $file ../regression-test-data/ne0_15x2_SA_gmted2010_modis_bedmachine_nc0540_Laplace0200_noleak_20230209.nc
+/glade/campaign/cesm/cesmdata/cprnc/cprnc -m $file ../regression-test-data/ne0_15x2_SA_gmted2010_modis_bedmachine_nc0540_Laplace0200_noleak_20241211.nc
 #/fs/cgd/csm/tools/cprnc/cprnc  -m $file ../regression-test-data/ne0_15x2_SA_gmted2010_bedmachine_nc0540_Laplace0200_20220610.nc
 #source plot.sh
--- cube_to_target/regr_test3.sh
+++ cube_to_target/regr_test3.sh
@@ -3,5 +3,5 @@ make
 ./cube_to_target --grid_descriptor_file='../regression-test-data/fv0.9x1.25.nc'  --intermediate_cs_name='../regression-test-data/gmted2010_bedmachine-ncube0540-220518.nc' --output_grid='fv0.9x1.25'  -u 'Peter Hjort Lauritzen, pel@ucar' -q 'output/' --smoothing_scale=100.0 
 set file=`ls -t1 output/*.nc |  head -n 1`
 echo $file
-/glade/p/cesm/cseg/tools/cprnc/cprnc -m $file ../regression-test-data/fv0.9x1.25_gmted2010_modis_bedmachine_nc0540_Laplace0100_noleak_20230209.nc
+/glade/campaign/cesm/cesmdata/cprnc/cprnc -m $file ../regression-test-data/fv0.9x1.25_gmted2010_modis_bedmachine_nc0540_Laplace0100_noleak_20241211.nc
 #/fs/cgd/csm/tools/cprnc/cprnc -m $file ../regression-test-data/fv0.9x1.25_gmted2010_bedmachine_nc0540_NoAniso_Laplace0100_20220610.nc
