aab01ef12f8d88284c1fff85d3d60805fc8b39fe
Peter Hjort Lauritzen <pel@ucar.edu>
2025-08-25 07:44:49 -0600

    update ne16pg3 script

--- cube_to_target/plot.sh
+++ cube_to_target/plot.sh
@@ -1,1 +1,1 @@
- ncl plot.ncl 'topoFile="output/ne30np4_gmted2010_modis_bedmachine_nc3000_Laplace0100_noleak_greenlndantarcsgh30fac2.50_20250822.nc"' 'scripFile="../regression-test-data/ne30pg3.nc"'
+ ncl plot.ncl 'topoFile="output/ne30np4_gmted2010_modis_bedmachine_nc3000_Laplace0100_noleak_greenlndantarcsgh30fac2.50_20250825.nc"' 'scripFile="../regression-test-data/ne30pg3.nc"'
